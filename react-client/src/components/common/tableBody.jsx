3e64e884608f8bdab25ca4d3f60e7ecba7aac3b1
Kevin Luang-orn <kluangorn@gmail.com>
2019-01-10 00:35:05 -0800

    table header

--- react-client/src/components/common/tableBody.jsx
+++ react-client/src/components/common/tableBody.jsx
@@ -2,15 +2,27 @@ import React, { Component } from "react";
 import _ from "lodash";
 
 class TableBody extends Component {
+  renderCell = (item, column) => {
+    if (column.content) return column.content(item);
+
+    return _.get(item, column.path);
+  };
+
+  createKey = (item, column) => {
+    return item._id + (column.path || column.key);
+  };
+
   render() {
     const { data, columns } = this.props;
 
     return (
       <tbody>
         {data.map(item => (
-          <tr>
+          <tr key={item._id}>
             {columns.map(column => (
-              <td>{_.get(item, columns.path)}</td>
+              <td key={this.createKey(item, column)}>
+                {this.renderCell(item, column)}
+              </td>
             ))}
           </tr>
         ))}
